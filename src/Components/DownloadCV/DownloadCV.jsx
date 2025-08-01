64e8292cc1eb501a2b26f748fa82c2549e2858c2
osama shehta <osama.shehtafathi@gmail.com>
2025-08-01 20:19:36 +0300

    Update DownloadCV component to use external link for CV download

--- src/Components/DownloadCV/DownloadCV.jsx
+++ src/Components/DownloadCV/DownloadCV.jsx
@@ -1,9 +1,9 @@
-import CV from "../../assets/Osama_Saqr_Frontend_Developer.pdf";
+// import CV from "../../assets/Osama_Saqr_Frontend_Developer.pdf";
 function DownloadCV() {
   return (
     <>
       <div className="flex justify-center items-center my-6">
-        <a href={CV} download="MyExampleDoc" target="_blank">
+        <a href='https://drive.google.com/file/d/1UF7l6MoAuvBOx2WIFuAF36fbdHqwbRgh/view?usp=sharing' download="MyExampleDoc" target="_blank">
           <button className="px-6 py-3 bg-slate-950 text-white dark:text-slate-950  dark:bg-white font-semibold rounded-lg hover:scale-110  transition-all duration-500">
             Download CV
           </button>
